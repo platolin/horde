8714f23f521d2edede68a678a3912a99429f1338
Michael J Rubinsky <mrubinsk@horde.org>
2015-05-14 18:17:27 -0400

    Horde::log should not be used outside of Core/Application code.

--- framework/Kolab_Storage/lib/Horde/Kolab/Storage/Data/Decorator/Log.php
+++ framework/Kolab_Storage/lib/Horde/Kolab/Storage/Data/Decorator/Log.php
@@ -572,6 +572,7 @@ implements Horde_Kolab_Storage_Data, Horde_Kolab_Storage_Data_Query
      */
     public function getQuery($name = null)
     {
-        return $this->_data->getQuery($name);
+        $query = $this->_data->getQuery($name);
+        $query->setLogger($this->_logger);
     }
 }
